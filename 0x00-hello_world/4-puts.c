b279a5ac4c3601d909fb8e6b25958702bc5b40c6
Godwin Moses Chukwudi <mosesgodwin18@gmail.com>
2023-02-16 15:31:26 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - to make the command puts work
+ *
+ * Return:  appear 0 after every succesful command
+ */
+
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
