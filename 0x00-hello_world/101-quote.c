5323fae93a10e95f3dfb4af058abe12d202ab903
Godwin Moses Chukwudi <mosesgodwin18@gmail.com>
2023-02-16 19:01:33 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - entry point
+*
+* Return: 1 upon sucess
+*/
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
