47384557e1648a13c36407a4323d3e70fb1499a7
Godwin Moses Chukwudi <mosesgodwin18@gmail.com>
2023-02-18 02:08:05 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the main emtey
+ * description - base coding
+ * Return: return 0 after succes
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x < 10 ; x++)
+		printf("%d", x);
+	printf("\n");
+	return (0);
+}
