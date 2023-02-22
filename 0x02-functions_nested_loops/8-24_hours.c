732378299698b40beb7ad6811f3a0bc546f91d17
Godwin Moses Chukwudi <mosesgodwin18@gmail.com>
2023-02-21 22:59:21 -0800

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - this is the proexixting function
+ *
+ * Return: return 0 after sucess
+ */
+
+void jack_bauer(void)
+{
+	int x;
+	int y;
+
+	for (x = 0 ; x < 24 ; x++)
+	{
+		for (y = 0 ; y < 60 ; y++)
+		{
+			_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+			_putchar(':');
+			_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
