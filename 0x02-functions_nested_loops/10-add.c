08ac6353a8a8a2710aa53eca5d00bbf695deafb3
Godwin Moses Chukwudi <mosesgodwin18@gmail.com>
2023-02-21 23:53:29 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - ads two numbers
+ *
+ * @a: parameter 1
+ * @b: parameter 2
+ *
+ * Return: return thesum of two parameters
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
