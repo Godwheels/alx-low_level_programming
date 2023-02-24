14a17ffefb3b3a118d550c96866de169c53d0918
Godwin Moses Chukwudi <mosesgodwin18@gmail.com>
2023-02-23 18:06:56 -0800

    interger mutiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplly two integers
+ * @a: firs function parameters
+ * @b: second functin parameters
+ * Return: return 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
