9ad8158c3fe5e055eb7b506acfedd256e8ebd14d
Godwin Moses Chukwudi <mosesgodwin18@gmail.com>
2023-02-23 17:28:33 -0800

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
  * _isupper - check if its upper case
- * @x: function parameter
+ * @c: function parameter
  * Return: return 1 for upper and 0 for any else
  */
 
 int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
