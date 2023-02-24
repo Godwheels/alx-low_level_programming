bed84545a6bb0e074d409625412e60bac3f87c12
Godwin Moses Chukwudi <mosesgodwin18@gmail.com>
2023-02-23 17:19:56 -0800

    is uper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check if its upper case
+ * @x: function parameter
+ * Return: return 1 for upper and 0 for any else
+ */
+
+int _isupper(int c)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
