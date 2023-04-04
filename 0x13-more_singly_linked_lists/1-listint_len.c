17ddaa3e546ace14959c2fb2cc100308f848c189
Godwin Moses Chukwudi <mosesgodwin18@gmail.com>
2023-04-04 10:22:54 -0700

    list length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,7 +3,6 @@
 /**
  * listint_len - returns the number of elements in a linked lists
  * @h: linked list of type listint_t to traverse
- * written by DONJOR
  * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
